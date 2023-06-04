f391382f63a7742d9161bc56ef21ba3591366ce9
Nutthawut Kongsopa <nutthawutkongsopa@gmail.com>
2023-06-04 13:10:31 +0700

    exit package.json

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "NSVueFlatpickr",
-      fileName: "ns-vue-flatpickr",
+      fileName: "index",
     },
     rollupOptions: {
       external: ["vue"],
